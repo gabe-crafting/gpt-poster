8dab89f83a4afd0623006b5743f8ec982d64f704
gabe-crafting <gabriel.projects90@proton.me>
2025-06-01 14:53:41 +0300

    last update

--- components/TooledTextarea.vue
+++ components/TooledTextarea.vue
@@ -6,16 +6,16 @@ interface TooledTextareaProps {
   transcribedText?: string;
 }
 
-const value = defineModel<string | undefined>({
+const textElement = ref<HTMLTextAreaElement>();
+
+const value = defineModel<string>({
   required: true,
-  default: '',
+  default: '' as string,
 })
 
 const props = withDefaults(defineProps<TextareaProps & TooledTextareaProps>(), {
   transcribedText: '',
 })
-
-const textElement = ref<HTMLTextAreaElement>();
 
 const handleSelect = (event: Event) => {
   textElement.value = event.target as HTMLTextAreaElement;
@@ -24,24 +24,33 @@ const handleSelect = (event: Event) => {
 const replaceText = () => {
   if (textElement.value) {
     const target = textElement.value;
-    const selectedText = target.value.substring(target.selectionStart, target.selectionEnd);
+    const start = target.selectionStart;
+    const end = target.selectionEnd;
+    const selectedText = target.value.substring(start, end);
     const currentText: string = value.value as string;
+
     if (selectedText) {
       console.log(props.transcribedText)
       // const transcribedText = props
       // // Update the value with the selected text
       if (value.value !== undefined) {
-        value.value = currentText.slice(0, target.selectionStart) +
-            props.transcribedText +
-            currentText.slice(target.selectionEnd);
+        value.value = currentText.slice(0, start) +
+        props.transcribedText +
+        currentText.slice(end) as typeof value.value;
       }
+
+      return
     }
+
+    value.value = currentText.substring(0, start) +
+    props.transcribedText +
+    currentText.substring(target.selectionEnd) as typeof value.value;
   }
 }
 
 const clearText = () => {
-  if (value.value) {
-    value.value = '';
+  if (value.value !== undefined) {
+    value.value = "" as typeof value.value;
   }
 }
 
@@ -62,7 +71,7 @@ const {copy} = useClipboard({source: ""})
         <UButton icon="material-symbols:delete-rounded" @click="clearText()"/>
       </UTooltip>
     </UButtonGroup>
-    <UTextarea :ref="textElement" v-model="value" v-bind="props" @select="handleSelect"/>
+    <UTextarea v-model="value" v-bind="props" @select="handleSelect"/>
   </div>
 </template>
 
