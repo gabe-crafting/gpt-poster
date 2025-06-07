e0a2482bc73b52df784b702fa4b395ac636c78ff
gabe-crafting <gabriel.projects90@proton.me>
2025-06-07 17:22:17 +0300

    finish temporary text for landing page

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,42 @@
 
 <template>
   <div class="flex flex-col justify-center items-center h-screen">
-    <div class="text-center">
-      <h1>Welcome to right to bear farms</h1>
-      <p>We are a rural tech hub</p>
+    <div class="w-2/3 text-center">
+      <p>The Right to Bear Farms is just a placeholder name for now. We intend to choose our name and brand style in the
+        coming months.</p>
+
+      <p>What we're here to do is help you firewall yourself from the world. We mean providing information that is
+        crucial, genuine, actionable, and distinct from the generalized content typical of self-help channels. We're
+        working on various businesses, products, services, ideas, and research and development, all with the common
+        theme that they can be sold globally and online. Not only do we show you what we're doing, but we also invite
+        you to get involved in these projects directly.</p>
+
+      <p>We're a private, invite-only members club. To become a member, you first have to become a follower, engage with
+        us, learn about what we do, and see if we're useful to you. If you find our information, products, services, and
+        locations beneficial, then you may want to consider a premium subscription.</p>
+
+      <p>The core team: Anybody who can bring complementary skills, capital, and resources, and who has the right
+        mindset
+        and character, can join the core team. Typically, we'll want to get to know you over at least six months, maybe
+        longer. This will also depend on your proven track record and experience.</p>
+
+      <p>All people in our organization are expected to have good character. Once we get to know you, we will explain
+        exactly what that means.</p>
+
+      <p>Paid subscribers get access to premium information. In other words, more fine detail as to what we're doing and
+        how we're doing it, along with early access to our projects.</p>
+
+      <p>Followers can join us for free through our content. This is how you get to know us. Once you feel comfortable
+        with the value you're getting, please upgrade to a small paid subscription.</p>
+
+      <p>Some of the subjects and sectors we're focused on are independent living off-grid, growing your own food and
+        automated farming, security, privacy, sound money, healthcare, personal wellbeing, diet and more.</p>
+
+      <p>We have one physical base in Eastern Europe, and we're also in contact with asssociates in Asia and South
+        America for a secondary backup plan bases.</p>
+
+      <p>All followers and paid subscribers will be updated regularly through our email list or whichever method you
+        prefer to stay informed.</p>
       <h2>Our products:</h2>
     </div>
     <div class="products m-12">
@@ -23,5 +56,9 @@
   display: flex;
   justify-content: center;
   align-content: center;
+}
+
+p {
+  margin: 10px 0;
 }
 </style>
